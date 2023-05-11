bdd2b075df9b2c6e0491c96b95bf6c857dead7de
EmiGM2090 <125908667+EmiGM2090@users.noreply.github.com>
2023-05-11 12:29:29 -0600

    Egm291004 (#64)
    * Se añadio Nickname a Readme
    
    * Propuesta
    
    * Create Emiliano.txt
    
    * Apuntes Emiliano
    
    * Apuntes Emiliano
    
    * Datos Emiliano
    
    * Mensaje Emiliano
    
    * Examenes
    
    * Spring_Easy_Interiors
    
    * Delete easy_interiors directory
    
    * Actualizacion
    
    * add model Decoracion
    
    * add daos DecoracionDao, DecoracionDaoImp
    
    ---------
    
    Co-authored-by: Deusabraham7 <125908670+Deusabraham7@users.noreply.github.com>

---
+++ easy_interiors/src/main/java/com/easy_interiors/easy_interiors/dao/DecoracionDao.java
@@ -0,0 +1,16 @@
+package com.easy_interiors.easy_interiors.dao;
+
+import com.easy_interiors.easy_interiors.models.Decoracion;
+
+import java.util.List;
+
+public interface DecoracionDao {
+
+    List<Decoracion> getDecoraciones();  //Especifico que debe existir el metodo "getDecoraciones"
+
+    Decoracion getDecoracion(Long Id);  //Especifico que debe existir el metodo "getDecoracion" y recibe un parametro llamado Id
+
+    void setDecoracion(Decoracion decoracion);  //Especofico que debe existir el metodo "setDecoracion" y recibe un parametro tipo Decoracion de nombre decoracion
+
+
+}
